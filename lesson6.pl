ab46b071342b3e0b0390a7ef51a1485cbbedb5e1
Dominik Bachórz <63674185+bachorzdominik@users.noreply.github.com>
2024-04-23 19:46:15 +0200

    Learn Perl by Doing - Part1, Regular Expressions

---
+++ lesson6.pl
@@ -0,0 +1,44 @@
+# (1.3) Regular Expressions:
+# The Regex Wildcard . -> any character except newline, to match new line use modifier s,
+# Groups () -> grouping characters,
+# Quantifiers - Greedy and Non Greedy,
+# '+' one or more,
+# '*' zero or more,
+# .* is gredy; so it maches zero or more characters, as many as possible,
+# .*? is non-greedy; so it matches zero or more, as few as possible,
+# Escape Sequences: \d -> digit, \w -> word character, \s -> whitespace, \b -> word boundary, \S -> non-whitespace,
+use strict;
+use warnings;
+
+$|=1;
+
+sub main {
+    my $book = 'files/mymanjeeves.txt';
+    open(INPUT, $book) or die "File not found $book\n";
+    
+    while(my $line = <INPUT>) {
+        if ($line =~ /(s.*?n)/) {
+            print "$1\n";
+        }
+    }
+
+    close(INPUT);
+
+    print("-------------------\n");
+
+    my $text = 'I am 117 years old tomorrow.';
+    my $text2 = 'Iam117yearsold_tomorrow.';
+
+    if ($text =~ /(am\s\d+)/) {
+        print "Matched: '$1'\n";
+    }
+    if ($text =~ /(y\S*)/) {
+        print "Matched: '$1'\n";
+    }
+    if ($text2 =~ /(\w*)/) {
+        print "Matched: '$1'\n";
+    }
+
+}
+
+main();
